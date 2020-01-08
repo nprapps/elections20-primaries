0493721de22a0829be675804477b4f39b3fe4c68
thomaswilburn <twilburn@npr.org>
2020-01-08 11:03:45 -0600

    Add live refresh for result table

--- src/js/components/results-table/index.js
+++ src/js/components/results-table/index.js
@@ -10,10 +10,13 @@ require("./style.less")
 
 var ElementBase = require("../elementBase");
 
+var defaultRefresh = 15;
+
 class ResultsTable extends ElementBase {
 
   constructor() {
     super();
+    this.timeout = null;
   }
 
   attributeChangedCallback(attr, was, value) {
@@ -28,11 +31,18 @@ class ResultsTable extends ElementBase {
     return ["src"];
   }
 
-  async load(src) {
+  async load(src = this.getAttribute("src")) {
     var response = await fetch(src);
     if (response.status >= 300) return this.innerHTML = "No data for this race";
     var data = await response.json();
     this.innerHTML = template(data);
+    if (this.hasAttribute("live")) this.scheduleRefresh();
+  }
+
+  scheduleRefresh() {
+    if (this.timeout) clearTimeout(this.timeout);
+    var interval = this.hasAttribute("refresh") ? this.getAttribute("refresh") : defaultRefresh;
+    this.timeout = setTimeout(() => this.load(), interval * 1000);
   }
 
 }
