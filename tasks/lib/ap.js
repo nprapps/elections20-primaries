b0f44389f65c1b87fd48731ed2be320a9c95e46d
thomaswilburn <twilburn@npr.org>
2020-01-09 08:43:44 -0600

    Redeem API requests in parallel
    I mean, we're not Python or something

--- tasks/lib/ap.js
+++ tasks/lib/ap.js
@@ -29,10 +29,9 @@ var getResults = async function(options) {
   var tickets = issueTickets(races);
   // console.log(`${tickets.length} tickets issued for AP data`);
   var rawResults = [];
-  for (var ticket of tickets) {
-    var data = await redeemTicket(ticket, options);
-    if (data) rawResults.push(data);
-  }
+  var redeemed = tickets.map(ticket => redeemTicket(ticket, options));
+  var rawResults = await Promise.all(redeemed);
+  rawResults = rawResults.filter(r => r);
   return normalizeResults(rawResults, options.overrides);
 };
 
