ae3f591aae2ad923b37a6e0df8596a8df2a9856e
thomaswilburn <twilburn@npr.org>
2020-01-27 11:36:40 -0600

    Fixes #27

--- src/js/components/utils.js
+++ src/js/components/utils.js
@@ -1,14 +1,28 @@
 var apMonths = [ "Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec." ];
 
+var timezones = [
+  { re: /\(eastern/i, zone: "ET" },
+  { re: /\(central/i, zone: "CT" },
+  { re: /\(mountain/i, zone: "MT" },
+  { re: /\(pacific/i, zone: "PT" }
+];
+
 var formatAPDate = date => `${apMonths[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 var formatTime = function(date) {
   var h = date.getHours()
   var m = date.getMinutes().toString().padStart(2, "0");
   var suffix = h > 12 ? "PM" : "AM";
+  var offset = date.getTimezoneOffset() / 60;
+  var zone = "";
+  if (offset > 5 && offset < 9) {
+    var ts = date.toTimeString();
+    var match = timezones.filter(tz => ts.match(tz.re)).pop();
+    if (match) zone = " " + match.zone;
+  }
   if (h > 12) {
     h -= 12;
   }
-  return `${h}:${m} ${suffix}`;
+  return `${h}:${m} ${suffix}${zone}`;
 };
 
 var parseNPRDate = function(d) {
