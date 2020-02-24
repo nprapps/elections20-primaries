0dc3b4d88c07e1421d51e82d961d4fe770d5a972
thomaswilburn <twilburn@npr.org>
2020-02-24 15:08:06 -0600

    Adjust timing for AP-related cron tasks
    Primary chnage is to reduce updates on production from 4/min to 3/min, since we've added more developers.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
   grunt.registerTask("serve", "Start the dev server", ["connect:dev", "watch"]);
   grunt.registerTask("default", ["clean", "static", "serve"]);
   // server tasks
-  grunt.registerTask("local", "Run the server for testing events", ["sheets", "static", "connect:dev", "cron:15:local"]);
-  grunt.registerTask("deploy", "Deploy HTML to stage on a timer", ["sheets", "static", "publish", "cron:15:publish"]);
-  grunt.registerTask("deploy-live", "Deploy HTML to live on a timer", ["sheets", "static", "publish:live", "cron:15:publishLive"]);
+  grunt.registerTask("local", "Run the server for testing events", ["sheets", "static", "connect:dev", "cron:60:local"]);
+  grunt.registerTask("deploy", "Deploy HTML to stage on a timer", ["sheets", "static", "publish", "cron:30:publish"]);
+  grunt.registerTask("deploy-live", "Deploy HTML to live on a timer", ["sheets", "static", "publish:live", "cron:20:publishLive"]);
 };
