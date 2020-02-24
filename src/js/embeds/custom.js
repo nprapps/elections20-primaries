f52b1ff390b4e301a23a522c2cc5caeb4fd44ae1
thomaswilburn <twilburn@npr.org>
2020-02-24 12:35:25 -0600

    Add a custom embed page
    See also #7

---
+++ src/js/embeds/custom.js
@@ -0,0 +1,48 @@
+var $ = require("../lib/qsa");
+var Sidechain = require("@nprapps/sidechain");
+
+// all our potential custom elements
+
+require("../components/president-primary");
+require("../components/president-caucus");
+require("../components/standard-primary");
+require("../components/house-primary");
+require("../components/delegate-total");
+
+var search = new URLSearchParams(window.location.search);
+
+var guest = Sidechain.registerGuest();
+
+var placeholder = $.one(".placeholder");
+
+var elementMap = {
+  P: "president-primary",
+  C: "president-caucus",
+  H: "house-primary",
+  G: "standard-primary",
+  S: "standard-primary"
+};
+
+var race = search.get("race");
+if (!elementMap[race]) {
+  console.error("Race type doesn't have a matching display element");
+} else {
+  var tag = document.createElement(elementMap[race]);
+  tag.src = `../data/${search.get("data")}.json`;
+
+  if (search.has("live")) {
+    tag.setAttribute("live", "");
+  }
+
+  if (search.has("link")) {
+    tag.setAttribute("href", search.get("link"));
+  }
+
+  var row = placeholder.parentElement;
+  if (race == "P" || race == "C") {
+    placeholder.parentElement.replaceChild(tag, placeholder);
+  } else {
+    row.innerHTML = "";
+    row.appendChild(tag);
+  }
+}
