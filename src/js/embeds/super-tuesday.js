420d85bab4a0881173226924f6f9d9676c028135
alykat <ahurt@npr.org>
2020-02-24 10:16:19 -0500

    more work setting up the super-tuesday widget / results-multiple-condensed element

---
+++ src/js/embeds/super-tuesday.js
@@ -0,0 +1,7 @@
+// load custom element shim
+var Sidechain = require("@nprapps/sidechain");
+var guest = Sidechain.registerGuest();
+
+// load the various result elements
+require("../components/president-primary-multiple");
+require("../components/delegate-total");
