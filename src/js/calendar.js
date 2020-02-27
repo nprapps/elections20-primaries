a4211062d8ec0b45c7e4eb369835b88cc5a4f59a
alykat <ahurt@npr.org>
2020-02-26 23:02:54 -0500

    calendar: show offices up for future elections, but remove labels/links #65

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -6,10 +6,14 @@ var now = Date.now();
 stamped.forEach(function(event) {
   var timestamp = event.dataset.timestamp * 1;
   if (timestamp > now) {
-    var links = $.one(".links", event);
-    links.remove();
+    // var links = $.one(".links", event);
+    // links.remove();
     // if we just want liks to not be clickable
-    // var links = $("[href]", event);
-    // links.forEach(a => a.removeAttribute("href"));
+    var links = $("[href]", event);
+    links.forEach(a => a.removeAttribute("href"));
+    var resultsLabel = $(".results-label", event);
+    resultsLabel.forEach(a => a.remove());
+    var coverageLink = $(".coverage-link", event);
+    coverageLink.forEach(a => a.remove());
   }
 });
