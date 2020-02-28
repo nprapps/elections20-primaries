999ad05d4ebbe3d9f55eee2146247d1bc1b07441
thomaswilburn <twilburn@npr.org>
2020-02-28 17:41:23 -0600

    Clean up hash routing
    Should fix the back button behavior and make this code easier to maintain.

--- src/js/statePage.js
+++ src/js/statePage.js
@@ -41,6 +41,7 @@ $("[data-live-timestamp]").forEach(function(element) {
   }
 });
 
+// module hash routing code
 var modules = $(".module:not(.future)");
 var noResultsModule = $.one(".module.future");
 var exactParams = ["date", "office"];
@@ -54,42 +55,33 @@ var lazyLoad = function() {
   });
 };
 
-var onHashChange = function(e) {
-  // construct a filter object from the hash
-  // we do this using the built-in type for parsing URL search params
-  $(".race-calendar .active").forEach(el => el.classList.remove("active"));
-  var hash = window.location.hash.replace("#", "");
-  noResultsModule.classList.add("hidden");
-  if (!hash) {
-    // show the latest items
-    var moduleDates = modules.map(function(m) {
-      var attribute = m.dataset.date;
-      var [m, d, y] = attribute.split("/").map(Number);
-      var date = new Date(y, m - 1, d);
-      return { date, attribute };
-    }).sort((a, b) => a.date - b.date);
-    var latest = moduleDates.filter(d => d.date < now).pop();
-    if (!latest) {
-      // if nothing is there, show the no-results module and hide everything else
-      modules.forEach(m => m.classList.add("hidden"));
-      noResultsModule.classList.remove("hidden");
-      return;
-    }
-    var date = latest.attribute;
-    modules.forEach(function(module) {
-      module.classList.toggle("hidden", !!(module.dataset.date != date || module.dataset.counties));
-    })
-    lazyLoad();
+var showLatest = function() {
+  document.body.classList.remove("filtered");
+  var moduleDates = modules.map(function(m) {
+    var attribute = m.dataset.date;
+    var [m, d, y] = attribute.split("/").map(Number);
+    var date = new Date(y, m - 1, d);
+    return { date, attribute };
+  }).sort((a, b) => a.date - b.date);
+  var latest = moduleDates.filter(d => d.date < now).pop();
+  if (!latest) {
+    // if nothing is there, show the no-results module and hide everything else
+    modules.forEach(m => m.classList.add("hidden"));
+    noResultsModule.classList.remove("hidden");
     return;
   }
-  $(`.race-calendar [href="#${hash}"]`).forEach(el => el.closest("li").classList.add("active"));
-  document.body.classList.add("filtered");
-  var params = new URLSearchParams(hash);
-  // set CSS hooks on the body
-  exactParams.forEach(p => document.body.dataset[p] = params.get(p));
-  booleanParams.forEach(p => document.body.classList.toggle(p, params.has(p)));
+  var date = latest.attribute;
+  var shown = modules.filter(function(module) {
+    var hidden = !!(module.dataset.date != date || module.dataset.counties);
+    module.classList.toggle("hidden", hidden);
+    return !hidden;
+  });
+  return shown;
+};
+
+var showMatching = function(params) {
   // now filter visible modules by looking for matching data params
-  modules.forEach(function(module) {
+  var matched = modules.filter(function(module) {
     var visible = true;
     for (var p of exactParams) {
       if (params.get(p) == "true" && !(p in module.dataset)) {
@@ -107,17 +99,41 @@ var onHashChange = function(e) {
       if (module.dataset.special) visible = false;
     }
     module.classList.toggle("hidden", !visible);
-    // find things to toggle party
-    $(`[data-control="party"]`, module).forEach(function(element) {
-      if (params.has("party")) {
-        element.setAttribute("party", params.get("party"));
-      } else {
-        element.removeAttribute("party");
-      }
-    })
+    return visible;
   });
+  return matched;
+};
 
-  // send focus to the top-most module if this came from a click
+var onHashChange = function(e) {
+  // construct a filter object from the hash
+  // we do this using the built-in type for parsing URL search params
+  $(".race-calendar .active").forEach(el => el.classList.remove("active"));
+  var hash = window.location.hash.replace("#", "");
+  // console.log(e, hash);
+  var params = new URLSearchParams(hash);
+  // set CSS hooks on the body
+  exactParams.forEach(p => document.body.dataset[p] = params.get(p));
+  booleanParams.forEach(p => document.body.classList.toggle(p, params.has(p)));
+  noResultsModule.classList.add("hidden");
+  // decide which way we go
+  if (!hash) {
+    showLatest();
+  } else {
+    $(`.race-calendar [href="#${hash}"]`).forEach(el => el.closest("li").classList.add("active"));
+    document.body.classList.add("filtered");
+    showMatching(params);
+  }
+
+  // find things to toggle party
+  $(`[data-control="party"]`).forEach(function(element) {
+    if (params.has("party")) {
+      element.setAttribute("party", params.get("party"));
+    } else {
+      element.removeAttribute("party");
+    }
+  });
+  
+  // send focus to the top-most module if this came from user interaction
   if (e) {
     if (params.has("counties") && !params.has("state")) {
       var countyModule = $.one(`.module:not(.hidden)[data-counties="true"]`);
@@ -129,6 +145,7 @@ var onHashChange = function(e) {
       if (headline) headline.focus();
     }
   }
+
   lazyLoad();
 }
 
